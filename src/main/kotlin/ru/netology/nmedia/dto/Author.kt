11e272b496fc284073d7ca47542af871c18e2fb6
kosiginiv83 <kosiginiv83@gmail.com>
2021-02-27 21:25:51 +0800

    Updated

---
+++ src/main/kotlin/ru/netology/nmedia/dto/Author.kt
@@ -0,0 +1,8 @@
+package ru.netology.nmedia.dto
+
+
+data class Author(
+    val id: Long,
+    val name: String,
+    val avatar: String,
+)
---
+++ src/main/kotlin/ru/netology/nmedia/repository/AuthorRepository.kt
@@ -0,0 +1,7 @@
+package ru.netology.nmedia.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import ru.netology.nmedia.entity.AuthorEntity
+
+
+interface AuthorRepository : JpaRepository<AuthorEntity, Long>
